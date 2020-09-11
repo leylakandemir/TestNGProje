ef37fdcfa3a64a4d223ace1aea94d68db35c9295
Leyla Kandemir <leylakandemir00@gmail.com>
2020-09-11 14:05:54 +0200

    ders13

--- src/test/java/com/techproed/tests/ActionClassTest.java
+++ src/test/java/com/techproed/tests/ActionClassTest.java
@@ -1,4 +1,60 @@
 package com.techproed.tests;
 
-public class ActionClassTest {
+import com.techproed.utilities.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+import javax.swing.*;
+
+public class ActionClassTest extends TestBase {
+    @Test
+    public void sagTiklama(){
+        driver.get("https://the-internet.herokuapp.com/context_menu");
+        WebElement element = driver.findElement(By.id("hot-spot"));
+        Actions actions = new Actions(driver);
+
+        //bir webelemente sag tiklamak
+        //action class ile islem yaparsaniz her seferinde perform() yazmalisiniz
+        actions.contextClick(element).perform();
+
+    }
+
+    @Test
+    public void ciftTiklama(){
+        driver.get("http://demo.guru99.com/test/simple_context_menu.html");
+        WebElement element = driver.findElement(By.xpath("//button[@ondblclick='myFunction()']"));
+        Actions actions = new Actions(driver);
+        actions.doubleClick(element).perform();
+    }
+    @Test
+    public void hoverOver(){
+        driver.get("http://www.amazon.com");
+        WebElement element = driver.findElement(By.id("nav-link-accountList"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(element).perform();
+    }
+
+    @Test
+    public void asagiYukari(){
+        driver.get("http://www.amazon.com");
+        Actions actions = new Actions(driver);
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        actions.sendKeys(Keys.END).perform();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        actions.sendKeys(Keys.PAGE_UP).perform();
+        actions.sendKeys(Keys.ARROW_UP).perform();
+        actions.sendKeys(Keys.ARROW_DOWN).perform();
+    }
 }
